e908bd2090f84b848c2ebae4bab49736f6ff0c6c
Aliga10 <aliga.duo.aliga@gmail.com>
2025-08-23 00:14:26 +0900

    meng edit

--- main.js
+++ main.js
@@ -16,8 +16,7 @@ person.name = 'jhon'
 console.log(person.name);
 
 // bracket notation
-person['name'] = 'marry';
-console.log(person['name']);
+person['name'] = 'marry'; 
 
 //performing a task
 function greet(firstName,lastName){
@@ -97,3 +96,84 @@ if (hour >= 6 && hour < 12 )
 else if ( hour >= 12 && hour < 18 )
     console.log('Good Afternoon!');
 else console.log('Good Evening!');
+
+// loop
+for (let i = 0 ; i <= 5 ; i++) {
+    if (i % 2 !== 0 ) console.log (i)
+}
+
+let i = 0;
+while (i <= 5 ){
+    if (i % 2 == 0 ) console.log(i);
+    i++;
+}
+
+// for in loop 
+
+// const person ={
+//     name : aliga ,
+//     age : 20 
+// };
+
+for ( let key in person )
+    console.log (key , person[key]);
+
+const car = ['toyota' ,'honda','bmw']
+for (let index in car ){
+    console.log ( index, car[index]);
+}
+let x = 0;
+while ( x < 10){
+    if (x === 6 ) break;
+    console.log(x);
+    x++;
+}
+
+// exercise
+
+// let width = 10;
+// let hight = 20 ;
+function isLancape ( width , hight ){
+    if ( width > hight ) console.log ('lanscape');
+    else console.log('potrait');
+    
+}
+isLancape(10,20);
+
+isLancape(25,16);
+
+// fizzbuzz exercise
+// if dixisible by 3 => fizz
+// if divisible by 5 => buzz
+// if divisible by both 3 and 5 => fizzbuzz
+// if cannot divisible => return the number
+// if input not a number => return not a number 
+
+// const output = fizzbuzz(3)
+// console.log(output);
+fizzbuzz('3')
+
+function fizzbuzz(input){
+    if (typeof input !== 'number' ) {
+        console.log('Not a Number') 
+    }
+    else if (input % 3 === 0 && input % 5 === 0 ){
+        console.log('fizzbuzz');
+    }
+    else if (input % 3 === 0) {
+        console.log('fizz');
+    }
+    else if (input % 5 === 0){
+        console.log('buzz');
+    }
+    else console.log(input);
+}
+
+ halo (20)
+function halo (pesan){
+    if (pesan === 10)
+        return 'kwaii'
+    else return 'aliga' ;
+}
+console.log(halo(10));
+console.log(halo(20))
